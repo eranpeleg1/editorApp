223311611a6a5fd2d3773cb5dbf5ce67c063a5b0
eran peleg <32025731+eranpeleg1@users.noreply.github.com>
2019-05-15 11:52:57 +0300

    Update editorScript.js

--- editorScript.js
+++ editorScript.js
@@ -5,31 +5,38 @@ function getAppManifest() {
 }
 
 const ButtonDef =  {
-    componentType : 'wysiwyg.viewer.components.SiteButton',
+    componentType: 'wysiwyg.viewer.components.SiteButton',
     layout: {
         x: 100,
-        'y': 100,
-        'width': 200,
-        'height': 100
+        y: 100,
+        width: 128,
+        height: 40
     },
-    style: 'b1',
-    data: {
-        'type': 'LinkableButton',
-        'label': 'Button',
-        'link': null
+    data:{
+        label:'button',
+        link:'',
+        metaData:{isPreset: false, schemaVersion: '1.0', isHidden: false},
+        type:'LinkableButton'
     },
-
+    'type': 'Component',
+    'props': {
+        margin:0,
+        align:'center',
+        metaData:  {isPreset: false, schemaVersion: '1.0', isHidden: false},
+        type: 'ButtonProperties'
+    }
 }
 
 async function install(editorSDK, appDefinitionId){
     const pageRef = await editorSDK.pages.getCurrent()
-    const compRef = await editorSDK.components.add(appToken, {componentDefinition: ButtonDef, pageRef: pageRef})
-    editorSDK.components.data.update(appToken, {componentRef: compRef, data: {label: '1'}})
-    editorSDK.components.data.update(appToken, {componentRef: compRef, data: {label: '2'}})
-    editorSDK.components.data.update(appToken, {componentRef: compRef, data: {label: '3'}})
+    const compRef = await editorSDK.components.add(appDefinitionId, {componentDefinition: ButtonDef, pageRef: pageRef})
+    editorSDK.components.data.update(appDefinitionId, {componentRef: compRef, data: {label: '1'}})
+    editorSDK.components.data.update(appDefinitionId, {componentRef: compRef, data: {label: '2'}})
+    await editorSDK.components.data.update(appDefinitionId, {componentRef: compRef, data: {label: '3'}})
 
-    const data = await editorSDK.components.data.get(appToken, {componentRef: compRef})
-    editorSDK.components.data.update(appToken, {componentRef: compRef, data: {label: data.label}})
+    const data = await editorSDK.components.data.get(appDefinitionId, {componentRef: compRef})
+    console.log('dataaaaa', data)
+    editorSDK.components.data.update(appDefinitionId, {componentRef: compRef, data: {label: data.label}})
 }
 
 async function editorReady(editorSDK, appDefinitionId, options) {
@@ -52,4 +59,3 @@ module.exports = {
     onEvent: onEvent,
     editorReady: editorReady,
     getAppManifest: getAppManifest
-};
